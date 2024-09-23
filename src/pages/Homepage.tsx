942324bddf211284bb24227864d8b8b2a9ba528b
level237 <Bramslevel129@gmail.com>
2024-09-23 17:43:05 +0100

    update header and hero section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,12 @@
 
+import Hero from '../components/Hero'
 import Header from '../components/Header'
 import React from 'react'
 
 export default function Homepage() {
   return (
     <div>
-      <Header/>
+      <Hero/>
     </div>
   )
 }
