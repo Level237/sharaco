909ae46bbe7eb9cd2de2337991e4e1e34f0a0b4e
level237 <Bramslevel129@gmail.com>
2024-10-19 00:18:54 +0100

    login server in action

--- src/api/auth/LoginServer.ts
+++ src/api/auth/LoginServer.ts
@@ -1,5 +1,17 @@
+import axios from "axios"
+import { getUrlApi } from "../getUrlApi"
 
 
-export const Login=(credentials:any)=>{
-    
+export const Login=(credentials:any)=>async(dispatch:any)=>{
+    try{
+        const response=await axios.post(`${getUrlApi}/api/login`,credentials)
+        const token=response.data.access_token;
+
+        console.log(token)
+        dispatch({type:'LOGIN_SUCCESS',payload:token})
+    }
+    catch(error:any){
+        console.log(error)
+        dispatch({type:'LOGIN_FAILURE',payload:error.response.data})
+    }
 }
