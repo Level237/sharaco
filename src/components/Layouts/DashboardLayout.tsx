76c2a2d41235b66e5de9e91802247f94ef84f738
level237 <Bramslevel129@gmail.com>
2024-10-20 20:17:42 +0100

    get token and user in persist data

--- src/components/Layouts/DashboardLayout.tsx
+++ src/components/Layouts/DashboardLayout.tsx
@@ -2,6 +2,13 @@ import React, { ReactNode } from 'react'
 import '../../assets/css/vendor.min.css'
 import '../../assets/css/theme.min-1.css'
 import '../../assets/css/theme-dark.min.css'
+import '../../assets/js/script.js'
+import '../../assets/js/hs.theme-appearance.js'
+
+import '../../assets/vendor/hs-navbar-vertical-aside/dist/hs-navbar-vertical-aside-mini-cache.js'
+
+import '../../assets/js/theme.min.js'
+
 
 
 
